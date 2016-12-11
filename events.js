15fa292a4f1e77f9f0551ac3d32fc10696a5ff74
j-west <westja89@gmail.com>
2016-12-10 21:09:43 -0600

    Add event listeners to the four buttons, each one adds a class with classList.add.  Created the classes being added.  Made the first section's text bold with by using firstElementChild on the article element

--- events.js
+++ events.js
@@ -1,19 +1,41 @@
 var outputTarget = document.querySelector("#output-target")
 var header1 = document.querySelector("h1")
 var inputField = document.querySelector("input")
+var guineaPig = document.querySelector("#guinea-pig")
 
 document.querySelector("article").addEventListener("click", (e) => {
   outputTarget.innerHTML = `<p>You clicked on the ${e.target.innerHTML} section`
+    inputField.value = ""
 })
 
 header1.addEventListener("mouseover", () => {
   outputTarget.innerHTML = "You moved your mouse over the header"
+  inputField.value = ""
 })
 
 header1.addEventListener("mouseout", () => {
   outputTarget.innerHTML = "You left me!!"
+  inputField.value = ""
 })
 
 inputField.addEventListener("input", (e) => {
   outputTarget.innerHTML = inputField.value
 })
+
+document.querySelector("#add-color").addEventListener("click", (e) => {
+  guineaPig.classList.add("color-blue")
+})
+
+document.querySelector("#make-large").addEventListener("click", (e) => {
+  guineaPig.classList.add("large-text")
+})
+
+document.querySelector("#add-border").addEventListener("click", (e) => {
+  guineaPig.classList.add("add-border")
+})
+
+document.querySelector("#add-rounding").addEventListener("click", (e) => {
+  guineaPig.classList.add("rounded")
+})
+
+document.querySelector("article").firstElementChild.classList.add("bold-text")
