cb556e937838385b9d2cd271d6d5f245d022dcd7
j-west <westja89@gmail.com>
2016-12-10 20:53:15 -0600

    Add event listener to the input element, when the function in the lsitener is fired, the outputTarget is set to the value of the inputField

--- events.js
+++ events.js
@@ -1,5 +1,7 @@
 var outputTarget = document.querySelector("#output-target")
 var header1 = document.querySelector("h1")
+var inputField = document.querySelector("input")
+
 document.querySelector("article").addEventListener("click", (e) => {
   outputTarget.innerHTML = `<p>You clicked on the ${e.target.innerHTML} section`
 })
@@ -10,4 +12,8 @@ header1.addEventListener("mouseover", () => {
 
 header1.addEventListener("mouseout", () => {
   outputTarget.innerHTML = "You left me!!"
+})
+
+inputField.addEventListener("input", (e) => {
+  outputTarget.innerHTML = inputField.value
 })
