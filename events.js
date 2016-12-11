9a42d82b92a1c8253d0bd37482159312d13548e0
j-west <westja89@gmail.com>
2016-12-10 20:48:08 -0600

    Added event listeners to the h1 tag for mouseover and mouseout.  Each listener's function changes outputTarget to the required text

--- events.js
+++ events.js
@@ -1,5 +1,13 @@
 var outputTarget = document.querySelector("#output-target")
-
+var header1 = document.querySelector("h1")
 document.querySelector("article").addEventListener("click", (e) => {
   outputTarget.innerHTML = `<p>You clicked on the ${e.target.innerHTML} section`
+})
+
+header1.addEventListener("mouseover", () => {
+  outputTarget.innerHTML = "You moved your mouse over the header"
+})
+
+header1.addEventListener("mouseout", () => {
+  outputTarget.innerHTML = "You left me!!"
 })
